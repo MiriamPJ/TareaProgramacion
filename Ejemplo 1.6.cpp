2ddbf721a71ca34e5205560d0e61f67eea399a1c
MiriamPJ <43543663+MiriamPJ@users.noreply.github.com>
2024-04-11 11:10:29 -0500

    Add files via upload

---
+++ Ejemplo 1.6.cpp
@@ -0,0 +1,33 @@
+#include "iostream"
+
+using namespace std;
+
+int main()
+{	
+	int A,B;
+	float RES;
+    //Mensaje de bienvenida
+    cout << "Hola! Este programa 1.7 Escribir el resultado de la expresion" << "\n";
+
+    //Se declaran los números que se sumarán (pueden ser decimales)
+    
+
+     //Se pide el primer numero
+    cout << "Por favor ingrese el valor de A:  " << "\n";
+
+    //Se asigna el primer valor a A
+    cin >> A;
+
+    //Se pide el segundo numero
+    cout << "Por favor ingrese el valor B: " << "\n";
+
+    //Se asigna el segundo valor a B
+    cin >> B;
+
+	RES=((A+B)*(A+B))/3;
+	
+    //Se muestra el resultado.
+    cout << "EL RESULTADO DE LA EXPRESION ES  " << RES <<"\n";
+
+   return 0;
+}
